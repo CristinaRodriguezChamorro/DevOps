9218b4dc717e96c216af9cb2c3dab7458c892aae
CristinaRodriguezChamorro <ec2-user@ip-172-31-31-177.ec2.internal>
2021-01-23 18:39:08 +0000

    new

--- todos/test.py
+++ todos/test.py
@@ -1,22 +1,25 @@
+    
+    
 import os
 import json
-
 import boto3
+
 translate = boto3.client('translate')
 dynamodb = boto3.client('dynamodb')
 firehose = boto3.client('firehose')
 TABLE_NAME = os.getenv('TABLE_NAME')
 
-def lambda_handler(event, context):
-
-    # fetch todo from the database
-    id = event['pathParameters']['id']
-    language = event['pathParameters']['language']
+def funcion(language):
+    id = "lkjlfksjd"
     trans = translate.translate_text(Text="Hello",
-             SourceLanguageCode='en', TargetLanguageCode=language)
+                 SourceLanguageCode='en', TargetLanguageCode=language)
 
-    responses = {
+    response = {
         "statusCode": 200,
         "body":  json.dumps(trans.get('TranslatedText')) 
     }
+
+
     return response
+    
+print(funcion("de"))
